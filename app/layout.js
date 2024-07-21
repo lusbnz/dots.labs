d57904114a7624c6a5c7f564c00b7a883aa019a6
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-07-21 20:02:42 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Acme } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const acme = Acme({
+const inter = Inter({
   weight: "400",
   subsets: ["latin"],
 });
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={acme.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
