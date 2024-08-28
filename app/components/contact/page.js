959ee7b0cc963e2d201f1ae8462e8409ad4e74ff
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-08-29 00:38:32 +0700

    update

--- app/components/contact/page.js
+++ app/components/contact/page.js
@@ -5,7 +5,9 @@ import Image from "next/image";
 import ActionButton from "../layout/ActionButton";
 import gsap from "gsap";
 import B from "@/public/icons/B.svg";
+import Mask from "@/public/images/mask-group.png";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./style.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,97 +17,16 @@ const Contact = ({ containerRef }) => {
   const imageRef = useRef(null);
   const formRef = useRef(null);
 
-  // useEffect(() => {
-  //   const section = containerRef.current;
-
-  //   gsap.fromTo(
-  //     charRef.current,
-  //     { scale: 0, opacity: 0 },
-  //     {
-  //       duration: 1,
-  //       scale: 1,
-  //       opacity: 1,
-  //       ease: "expo.inOut",
-  //       scrollTrigger: {
-  //         trigger: section,
-  //         start: "top center",
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
-  //   gsap.fromTo(
-  //     titleRef.current,
-  //     { scale: 0, opacity: 0 },
-  //     {
-  //       duration: 1,
-  //       scale: 1,
-  //       opacity: 1,
-  //       ease: "expo.inOut",
-  //       scrollTrigger: {
-  //         trigger: section,
-  //         start: "top center",
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
-
-  //   gsap.fromTo(
-  //     imageRef.current,
-  //     { x: 100, opacity: 0 },
-  //     {
-  //       duration: 1,
-  //       x: 0,
-  //       opacity: 1,
-  //       ease: "expo.inOut",
-  //       scrollTrigger: {
-  //         trigger: section,
-  //         start: "top center",
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
-
-  //   gsap.fromTo(
-  //     formRef.current.children,
-  //     { y: 50, opacity: 0, stagger: 0.2 },
-  //     {
-  //       duration: 0.8,
-  //       y: 0,
-  //       opacity: 1,
-  //       stagger: 0.2,
-  //       ease: "expo.inOut",
-  //       scrollTrigger: {
-  //         trigger: section,
-  //         start: "top center",
-  //         toggleActions: "play none none reverse",
-  //       },
-  //     }
-  //   );
-  // }, []);
-
   return (
     <div>
       <BreadCumb title={"LIÊN HỆ"} />
-      <div ref={containerRef} className="relative h-[100vh] p-20">
-        <Image
-          ref={charRef}
-          src={B}
-          alt="B"
-          style={{
-            width: "360px",
-            height: "300px",
-            position: "absolute",
-            top: "-160px",
-            left: "120px",
-          }}
-        />
+      <div ref={containerRef} className="relative min-h-[100vh] p-20">
+        <Image ref={charRef} src={B} alt="B" className="text-1"/>
         <span
           ref={titleRef}
-          className="absolute"
+          className="text-2"
           style={{
-            fontSize: "170px",
-            top: "-48px",
-            textIndent: "360px",
+            position: "absolute",
             fontFamily: "PPRader",
           }}
         >
@@ -113,12 +34,9 @@ const Contact = ({ containerRef }) => {
         </span>
         <span
           ref={titleRef}
-          className="absolute"
+          className="text-3"
           style={{
-            fontSize: "170px",
-            top: "160px",
-            left: "50px",
-            // textIndent: "360px",
+            position: "absolute",
             fontFamily: "PPRader",
           }}
         >
@@ -126,11 +44,9 @@ const Contact = ({ containerRef }) => {
         </span>
         <span
           ref={titleRef}
-          className="absolute"
+          className="text-4"
           style={{
-            fontSize: "170px",
-            top: "360px",
-            textIndent: "580px",
+            position: "absolute",
             fontFamily: "PPRader",
           }}
         >
@@ -138,37 +54,32 @@ const Contact = ({ containerRef }) => {
         </span>
         <Image
           ref={imageRef}
-          src={"/images/mask-group.png"}
-          width={200}
-          height={136}
+          src={Mask}
           alt="contact"
-          style={{ position: "absolute", left: "60px", top: "420px" }}
+          className="absolute side-image"
         />
-        <span
-          className="absolute"
-          style={{ fontSize: "16px", top: "600px", maxWidth: "300px" }}
-        >
+        <span className="absolute text-5">
           Hãy cung cấp những thông tin dưới dây chúng tôi sẽ liên hệ bạn sớm
           nhất!
         </span>
         <div
           ref={formRef}
-          className="absolute flex flex-col right-[34%] top-[60%] gap-2"
+          className="absolute flex flex-col form-container"
         >
           <input
             type="text"
             placeholder="Họ và tên"
-            className="w-[600px] h-[80px] pl-[28px] rounded-[12px] bg-[#191919]"
+            className="form-item bg-[#191919]"
           />
           <input
             type="text"
             placeholder="Email / SDT"
-            className="w-[600px] h-[80px] pl-[28px] rounded-[12px] bg-[#191919]"
+            className="form-item bg-[#191919]"
           />
           <input
             type="text"
             placeholder="Nhu cầu cần tư vấn"
-            className="w-[600px] h-[80px] pl-[28px] rounded-[12px] bg-[#191919] mb-4"
+            className="form-item bg-[#191919] end"
           />
           <ActionButton index={2} />
         </div>
